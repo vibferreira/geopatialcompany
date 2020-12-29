d40cc6f7462a9767566e352c548b56ade79462db
Vitoria Barbosa Ferreira <ferreiravitoriabarbosa@gmail.com>
2020-12-29 05:28:34 -0300

    Setting the basemap

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// initialize the map. The global variable stores the map object for subsequent reference
+var map;
+
+map = L.map ('map', {
+		center: [47.8095, 13.0550],
+		zoom: 4
+    });
+
+// adding the tile layers (basemaps) to the map 
+
+var Dark_Streets = L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	minZoom: 0,
+	maxZoom: 22,
+	subdomains: 'abcd',
+	accessToken: '2aGuIHpH6H1xqQq2ly21G8ecFhmzxpf7NydCHPRwyEKgvQCmhkrUDAtTBCo4jkxw'
+});
+
+var streets =  L.tileLayer('https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	minZoom: 0,
+	maxZoom: 22,
+	subdomains: 'abcd',
+	accessToken: '2aGuIHpH6H1xqQq2ly21G8ecFhmzxpf7NydCHPRwyEKgvQCmhkrUDAtTBCo4jkxw'
+}).addTo(map);
+
+
+// this variable will be used in the layer control (at the end)
+var baseMaps = {
+	"Streets Dark": Dark_Streets,
+	"Streets Light": streets
+}
+
+// adding scale bar
+L.control.scale({position:'bottomleft', imperial:false}).addTo(map);
+
+// adding north arrow
+var north = L.control({position: "bottomleft"});
+north.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    div.innerHTML = '<img src= "data/north-arrow.png" alt="Image" height = "30" width="30">';
+    return div;
+}
+north.addTo(map);
+
+//adding sidebar
+var sidebar = L.control.sidebar('sidebar', 'left').addTo(map);
+
+// adding GeoJSON point features to the map 
+
+
+
+
+
+
+	
+//adding Layer control
+L.control.layers(baseMaps, 
+	{position:'topright', collapsed: true}).addTo(map); //layer control not working for the moment
