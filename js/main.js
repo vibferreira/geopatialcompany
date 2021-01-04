540d979ea8a6dd391e5ce52c11fee5e5305371c8
Pratichhya <mepratichhya@gmail.com>
2021-01-04 15:45:46 +0100

    search bar added to the map

--- js/main.js
+++ js/main.js
@@ -91,6 +91,19 @@ var geoLayer=L.esri.Cluster.featureLayer({
 			)}
 	});
 geoLayer.addTo(map);
+// create the geocoding control and add it to the map
+var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+// create an empty layer group to store the results and add it to the map
+var results = L.layerGroup().addTo(map);
+
+// listen for the results event and add every result to the map
+searchControl.on("results", function (data) {
+  results.clearLayers();
+  for (var i = data.results.length - 1; i >= 0; i--) {
+	results.addLayer(L.marker(data.results[i].latlng));
+  }
+});
 
 
 //adding Layer control
