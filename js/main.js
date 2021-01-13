4507ab08df0cf30885ca9486452fcf154b0f378a
Pratichhya <mepratichhya@gmail.com>
2021-01-13 13:29:55 +0100

    combined

--- js/main.js
+++ js/main.js
@@ -1,11 +1,89 @@
 // initialize the map. The global variable stores the map object for subsequent reference
-var map;
+var lat = 25.694330;
+var lng = -15.459268;
+var zoom = 2.4;
+var map = L.map('map', {
+  zoomControl: false
+});
 
-map = L.map ('map', {
-		center: [25.694330, -15.459268],
-		zoom: 2.4
-    });
+map.setView([lat, lng], zoom);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18
+}).addTo(map);
 
+// custom zoom bar control that includes a Zoom Home function
+L.Control.zoomHome = L.Control.extend({
+  options: {
+      position: 'topleft',
+      zoomInText: '<i class="fa fa-search-plus fa-1x" ></i>',
+      zoomInTitle: 'Zoom in',
+      zoomOutText:'<i class="fa fa-search-minus fa-1x" ></i>',
+      zoomOutTitle: 'Zoom out',
+      zoomHomeText: '<i class="fa fa-home fa-1x" style="line-height:1.5;"></i>',
+      zoomHomeTitle: 'Zoom home'
+  },
+
+  onAdd: function (map) {
+      var controlName = 'gin-control-zoom',
+          container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
+          options = this.options;
+
+      this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
+      controlName + '-in', container, this._zoomIn);
+      this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
+      controlName + '-home', container, this._zoomHome);
+      this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
+      controlName + '-out', container, this._zoomOut);
+
+      this._updateDisabled();
+      map.on('zoomend zoomlevelschange', this._updateDisabled, this);
+
+      return container;
+  },
+  onRemove: function (map) {
+      map.off('zoomend zoomlevelschange', this._updateDisabled, this);
+  },
+  _zoomIn: function (e) {
+      this._map.zoomIn(e.shiftKey ? 3 : 1);
+  },
+  _zoomOut: function (e) {
+      this._map.zoomOut(e.shiftKey ? 3 : 1);
+  },
+  _zoomHome: function (e) {
+      map.setView([lat, lng], zoom);
+  },
+  _createButton: function (html, title, className, container, fn) {
+      var link = L.DomUtil.create('a', className, container);
+      link.innerHTML = html;
+      link.href = '#';
+      link.title = title;
+
+      L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
+          .on(link, 'click', L.DomEvent.stop)
+          .on(link, 'click', fn, this)
+          .on(link, 'click', this._refocusOnMap, this);
+
+      return link;
+  },
+
+  _updateDisabled: function () {
+      var map = this._map,
+          className = 'leaflet-disabled';
+
+      L.DomUtil.removeClass(this._zoomInButton, className);
+      L.DomUtil.removeClass(this._zoomOutButton, className);
+
+      if (map._zoom === map.getMinZoom()) {
+          L.DomUtil.addClass(this._zoomOutButton, className);
+      }
+      if (map._zoom === map.getMaxZoom()) {
+          L.DomUtil.addClass(this._zoomInButton, className);
+      }
+  }
+});
+// add the new control to the map
+var zoomHome = new L.Control.zoomHome();
+zoomHome.addTo(map);
 // adding the tile layers (basemaps) to the map 
 
 var Dark_Streets = L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
